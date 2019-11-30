8c490a5fb939a20d1a9220f34d2571862ab4b3bd
CodeNameLuke <ltsilva@ucdavis.edu>
2019-11-29 22:50:28 -0800

    ?

--- ECS-HW4/ECS-HW4/maxTweeter.c
+++ ECS-HW4/ECS-HW4/maxTweeter.c
@@ -10,8 +10,9 @@
 #include "maxTweeter.h"
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
-
+// Include '\n' but exclude '\0'
 #define MAX_LINE_LENGTH 1024 // Line size will not exceed 1024 bytes.
 #define MAX_ROWS 20000 // 20,000 Lines Max CSV.
 
@@ -21,13 +22,13 @@ char* removeOutermostQuotes(char* string){
     int lastCharPosition = strlen(string) - 1;
     
     printf("First character = %c, Last Character = %c\n", string[0],string[lastCharPosition]);
-    
-    
-    
     return "hello";
     
 }
 
+
+// Gets number of columns from splitting the csv...
+// This must be called during every line read to check if the # of columns is the same for all lines.
 int getNumberOfColumns(char* line){
     
     int count = 0;
@@ -37,8 +38,10 @@ int getNumberOfColumns(char* line){
     
     char *word;
     
-    for(word = strtok(buffer, "'"); word && *word; word = strtok(NULL, ",\n"), count+=1 ){
-        // nothing
+    for(word = strtok(buffer, ","); word && *word; word = strtok(NULL, ",\n")){
+        
+        count +=1;
+        
     }
     return count;
 }
@@ -47,8 +50,9 @@ int getNumberOfColumns(char* line){
 // Also check if there are other "name" or ""name"" columns...making file invalid.
 int getNameColNumber(char *headerLine){
     
-    int colNum;
+    int colNum = 0;
     int count = 0;
+    int actualColNum = 0;
     // On piazza TA said that maximum size of name could by whole line?
     char possibleName1[1024];
     char possibleName2[1024];
@@ -62,6 +66,7 @@ int getNameColNumber(char *headerLine){
     for (colNum = 0, tok = strtok(headerLine, ",");tok && *tok;tok = strtok(NULL, ",\n"), colNum += 1){
         // Increment count for possible matches...
         if (strcmp(tok, possibleName1) == 0 || strcmp(tok, possibleName2) == 0){
+            actualColNum = colNum;
             count += 1;
         }
     }
@@ -71,23 +76,48 @@ int getNameColNumber(char *headerLine){
         return -1;
     }
 
-    return colNum;
+    return actualColNum;
 }
+
+int isValidCSV(const char *filename){
+    
+    // Check if the file actually ends in .csv.
+    // Return 0 if file is valid.
+    // Return -1 if file is invalid.
+    
+    return 0;
+    
+}
+
+void testFile(const char *filename){
+    
+    FILE *file = fopen(filename, "r");
+    
+    if(!file){
+        
+        perror("Invalid Input Format");
+        
+    }
+    
+}
+
 
 void maxTweeter(const char *filename){
 
-    FILE *file = fopen(filename, "r");
-    if(!file){
-        printf("Invalid Input Format");
-    }
+    printf("filename = %s\n", filename);
     
+    FILE *file = fopen(filename, "r");
+    
+    if(!file){
+        perror("Invalid Input Format");
+    }
     
     int row_count = 0;
     int column_count = 0;
     char *columnNames[1024];
     
     // Array to store all csv information.
-    char csvToArray[MAX_ROWS][MAX_LINE_LENGTH];
+//    char csvToArray[MAX_ROWS][MAX_LINE_LENGTH];
     // temp buffer to store line contents.
     char buffer[MAX_LINE_LENGTH];
     
@@ -98,10 +128,15 @@ void maxTweeter(const char *filename){
         // Check if first line is null.
         // Check for empty first line ... " ,   ,  ,,,  ,, , ,"
         
+        
         row_count++;
         
         // Process the header... check for "name" column.
         if(row_count == 1){
+            
+            column_count = getNumberOfColumns(buffer);
+            
+            printf("Column_count = %d\n", column_count);
             
             // Gets pointer to first element.
             columnNames[0] = strtok(buffer, ",");
@@ -128,10 +163,13 @@ void maxTweeter(const char *filename){
             continue;
             
         }
-    }
+        
+        
+    } // fgets()
     
     
-}
+} // maxTweeter
+
 
 int main(int argc, char *argv[]){
     
@@ -147,13 +185,8 @@ int main(int argc, char *argv[]){
 //    }
 //    char* fileName = argv[2];
     
-//    maxTweeter("/Users/LukeSilva/Downloads/cl-tweets-short-clean.csv");
-    
-    char* string = "YOUNGMONEYMILITIA";
-    
-    checkForQuotes(string);
-    
-    
+    testFile("‎⁨/Users/celestinosilva/Downloads/test-tweets.csv");
+
     return 0;
     
 }
